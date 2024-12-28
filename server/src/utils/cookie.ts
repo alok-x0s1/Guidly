5145579ccb2f9fd59fb9ec3bedc600607b9d7193
Alok Yadav <yalok6321@gmail.com>
2024-12-28 13:15:15 +0530

    change in cookie options

--- server/src/utils/cookie.ts
+++ server/src/utils/cookie.ts
@@ -2,7 +2,7 @@ import { config } from "../config/config";
 
 export const cookieOptions = {
 	httpOnly: true,
-	sameSite: true,
+	sameSite: config.node_env === "production" ? "none" : "lax",
 	secure: config.node_env === "production",
 	maxAge: 1000 * 60 * 60 * 24 * 7,
 };
